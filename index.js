a908ad850b1b82b37624e4adc28788060f0b17b7
apps221 <maggie.a.hong@gmail.com>
2025-02-12 15:05:28 -0500

    finished

--- index.js
+++ index.js
@@ -6,16 +6,23 @@ async function movieSearch() {
     const inputValue = input.value;
     const movies = await fetch(`https://omdbapi.com/?s=${inputValue}&apikey=5b537eef`);
     const moviesData = await movies.json();
-  movieList.innerHTML = moviesData.map(movie => 
-        ` <div class="movie-card__container">
-                  <h3>Title</h4>
-                <p><b>Year: </b>0000</p>
-                <p><b>imbID: </b>000000000</p>
-                <p><b>Type: </b>Undefined</p>
-                <p><b>Poster: </b>
-                <img src="" alt="" class="movie__img">
+   const moviesFive = moviesData.Search.slice(0,5);
+   console.log(moviesFive);
+    movieList.innerHTML = moviesFive.map(movie => 
+        `<div class="movie-list">
+            <div class="movie">
+              <div class="movie-card">
+                <div class="movie-card__container">
+                  <h3>${movie.Title}</h4>
+                <p><b>Year: </b>${movie.Year}</p>
+                <p><b>imbID: </b>${movie.imdbID}</p>
+                <p><b>Type: </b>${movie.Type}</p>
+                <img src="${movie.Poster}" alt="" class="movie__img">
                 </p>
-              </div>`
+              </div>
+            </div>
+          </div>
+        </div>`
     ).join("");
 }
 
