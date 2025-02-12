d58e8a1b0348fb856a653093e7db2c9fe1141bfa
apps221 <maggie.a.hong@gmail.com>
2025-02-12 13:35:23 -0500

    finished search

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // https://omdbapi.com/?s=fast&apikey=5b537eef
 const input = document.getElementById('myInput');
-function movieSearch(input) {
-    console.log(input);
+function movieSearch() {
+    console.log(input.value);
 }
-movieSearch();
+
