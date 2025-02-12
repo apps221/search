355e362f3e96b42c9b513da688b27335a00f1a97
apps221 <maggie.a.hong@gmail.com>
2025-02-12 13:49:31 -0500

    search almost done?

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // https://omdbapi.com/?s=fast&apikey=5b537eef
 const input = document.getElementById('myInput');
-function movieSearch() {
-    console.log(input.value);
+//remember to use input.value to access input text
+async function movieSearch() {
+    const inputValue = input.value;
+    const movies = await fetch(`https://omdbapi.com/?s=${inputValue}&apikey=5b537eef`);
+    const moviesData = await movies.json();
+
 }
 
