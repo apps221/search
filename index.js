b8624a558813dda189bd5140f175b387b163c227
apps221 <maggie.a.hong@gmail.com>
2025-02-12 14:06:26 -0500

    next update

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 // https://omdbapi.com/?s=fast&apikey=5b537eef
+const movieList = document.querySelector('.movie-list');
 const input = document.getElementById('myInput');
 //remember to use input.value to access input text
 async function movieSearch() {
     const inputValue = input.value;
     const movies = await fetch(`https://omdbapi.com/?s=${inputValue}&apikey=5b537eef`);
     const moviesData = await movies.json();
-
+  movieList.innerHTML = moviesData.map(movie => 
+        ` <div class="movie-card__container">
+                  <h3>Title</h4>
+                <p><b>Year: </b>0000</p>
+                <p><b>imbID: </b>000000000</p>
+                <p><b>Type: </b>Undefined</p>
+                <p><b>Poster: </b>
+                <img src="" alt="" class="movie__img">
+                </p>
+              </div>`
+    ).join("");
 }
 
