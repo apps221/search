9ab3fbb281d48c11145df0343b39d4a7c8794ca9
apps221 <maggie.a.hong@gmail.com>
2025-02-13 12:51:41 -0500

    new scroll update

--- index.js
+++ index.js
@@ -7,25 +7,31 @@ async function movieSearch() {
     const inputValue = input.value;
     const movies = await fetch(`https://omdbapi.com/?s=${inputValue}&apikey=5b537eef`);
     const moviesData = await movies.json();
-    const moviesFive = moviesData.Search.slice(0, 5);
-    console.log(moviesFive);
-    movieList.innerHTML = moviesFive.map(movie => 
-        `<div class="movie-list">
-            <div class="movie">
-              <div class="movie-card">
-                <div class="movie-card__container">
-                  <h3>${movie.Title}</h4>
-                <p><b>Year: </b>${movie.Year}</p>
-                <p><b>imbID: </b>${movie.imdbID}</p>
-                <p><b>Type: </b>${movie.Type}</p>
-                <img src="${movie.Poster}" alt="" class="movie__img">
-                </p>
+    if(moviesData.Search) {
+      const moviesFive = moviesData.Search.slice(0, 5);
+      console.log(moviesFive);
+      movieList.innerHTML = moviesFive.map(movie => 
+          `<div class="movie-list">
+              <div class="movie">
+                <div class="movie-card">
+                  <div class="movie-card__container">
+                    <h3>${movie.Title}</h4>
+                  <p><b>Year: </b>${movie.Year}</p>
+                  <p><b>imbID: </b>${movie.imdbID}</p>
+                  <p><b>Type: </b>${movie.Type}</p>
+                  <img src="${movie.Poster}" alt="" class="movie__img">
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        </div>`
-    ).join("");
-    scrollToSection();
+          </div>`
+      ).join("");
+      scrollToSection();
+    }
+    else {
+      movieList.innerHTML = "<p>No Movies Found</p>";
+    }
+  
 }
 function scrollToSection() {
   targetSection.scrollIntoView({ behavior: 'smooth' });
